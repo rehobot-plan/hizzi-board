ff80d735d8d82ede0c2f2e49e2f949de21e2a43d
rehobot <oilpig85@gmail.com>
2026-04-03 20:11:43 +0900

    fix: next.config 캐시 제거 + todoRequests 필터링 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
